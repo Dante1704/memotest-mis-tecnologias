3d157c9c038d2fa65772cb975a61bbb29847dd9e
Dante <dante.nkaddarian@gmail.com>
2023-03-22 18:02:28 -0300

    tareas: indicar que se gano el juego cuando ya no quedan mas casilleros por descubrir y dar la posibilidad de jugar de nuevo volviendo a las condiciones iniciales

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 [x] crear el tablero de juego estatico dividiendola en la cantidad de tecnologias x2
 [x] renderizar el tablero con las imagenes de las tecnologias para cada casillero
 [x] cubrir los casilleros para que no se vean las tecnologias y mostrarlos cuando se los clickea
-[] crear la logica del juego comparando de a dos casilleros, si se acierta dejarlos visibles, sino ocultarlos
-[] indicar que se gano el juego cuando ya no quedan mas casilleros por descubrir
-[] dar la posibilidad de jugar de nuevo volviendo a las condiciones iniciales
+[x] crear la logica del juego comparando de a dos casilleros, si se acierta dejarlos visibles, sino ocultarlos
+[x] indicar que se gano el juego cuando ya no quedan mas casilleros por descubrir, por ahora con un alert, luego darle estilo
+[x] dar la posibilidad de jugar de nuevo volviendo a las condiciones iniciales
 [] cuando se juega de nuevo o cuando se monta el componente, generar el tablero al azar.
--- src/Components/Memotest.tsx
+++ src/Components/Memotest.tsx
@@ -1,15 +1,34 @@
 import getTechnologies from '../helpers/getTechnologies'
 import emoji from '../assets/emoji-confused.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const technologies: string[] = getTechnologies()
 
 function Memotest (): JSX.Element {
-  const [selected, setSelected] = useState<string[]>([''])
+  const [selected, setSelected] = useState<string[]>([])
+  const [guessed, setGuessed] = useState<string[]>([])
 
-  const handleClick = (tech: string): void => { // cuando lo clicleo, que valla a selected
-    setSelected((prev) => [...prev, tech])
+  useEffect(() => {
+    if (selected.length === 2) {
+      if (selected[0].split('|')[0] === selected[1].split('|')[0]) { // esto lo hago para que la comparacion sea igual
+        setGuessed((prev) => [...prev, ...selected])
+      }
+      setTimeout(() => { setSelected([]) }, 1000)
+    }
+  }, [selected])
+
+  useEffect(() => {
+    if (guessed.length === technologies.length) {
+      alert('ganaste!')
+      setTimeout(() => { setGuessed([]) }, 1000)
+    }
+  }, [guessed])
+
+  const handleClick = (tech: string): void => { // cuando lo clickeo, que vaya a selected
+    // si es menor que 2 que agregue sino, no. Las comparaciones son de a 2
+    if (selected.length < 2) setSelected((prev) => [...prev, tech])
   }
+
   return (
     <main className='h-[100vh] flex justify-center items-center'>
       <ul className='grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 max-w-[1200px] gap-10'>
@@ -17,11 +36,7 @@ function Memotest (): JSX.Element {
         const url = tech.split('|')[0]
         return (
           <li key={tech} className="flex justify-center items-center min-w-14" onClick={() => { handleClick(tech) }}>
-            {/* <div className={!isHidden ? 'flip-card-rotate flip-card-inner' : 'flip-card-inner'}>
-              <img src={url} className="w-12 h-12 sm:w-24 h-24 flip-card-back "/>
-              <img src={emoji} className="w-12 h-12 sm:w-24 h-24 flip-card-front"/>
-            </div> */}
-            { selected.includes(tech)
+            { selected.includes(tech) || guessed.includes(tech)
               ? <img src={url} className="w-12 h-12 sm:w-24 h-24"/> /* flip-card-front */
               : <img src={emoji} className="w-12 h-12 sm:w-24 h-24"/> /* flip-card-front */
             }
